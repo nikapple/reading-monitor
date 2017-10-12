5808dc2853953e37f29dfe9cf349c7db568925bc
Nikhil Pereira <nikhil.pereira@ideacrestsolutions.com>
2017-10-12 16:04:18 +0530

    Add Dao and Service interfaces for Book operations

---
+++ src/main/java/com/readingmonitor/service/BookService.java
@@ -0,0 +1,11 @@
+package com.readingmonitor.service;
+
+import java.util.List;
+
+import com.readingmonitor.dto.Book;
+import com.readingmonitor.dto.Topic;
+
+public interface BookService {
+	List<Topic> getAllTopics();
+	List<Book> getAllBooks();
+}
